b0b9d2d091715208340354649134e6210c0b3080
Leslie <51496611+leslieecv@users.noreply.github.com>
2021-11-17 16:55:47 -0300

    testing

--- test_IK.py
+++ test_IK.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('/home/pi/mlf-marie-kondo')
 from serial_control import SerialControl
 from mk2robot import MK2Robot
+from movement import Effector
 #from core.serial_control import SerialControl #for pc
 #from core.mk2robot import MK2Robot #for pc
 import time
@@ -9,6 +10,7 @@ import numpy as np
 
 robot = MK2Robot(link_lengths=[55, 39, 135, 147, 66.3])
 robot_serial = SerialControl()
+eff = Effector(80,40)
 
 robot_serial.open_serial()
 
@@ -24,7 +26,18 @@ time.sleep(1.2)
 robot_serial.run_effector(60)
 print("voy 3")
 time.sleep(1.2)
-robot_serial.run_effector(4)
+robot_serial.run_effector(40)
+
+q0, q1, q2 = robot.inverse_kinematics(X_poses[i], Y_poses[i], Z_poses[i])
+robot_serial.write_servo(1, q0 + 45)
+eff.open()
+time.sleep(1.2)
+robot_serial.write_servo(2, 90 - q1)
+eff.close()
+time.sleep(1.2)
+robot_serial.write_servo(3, q2 + q1)
+eff.open()
+time.sleep(1.2)
 
 print("termine")
 time.sleep(1.2)
