64683689b252eec3ae1696f77d3ae933b22232bf
EstebanMunoz <estebanminoz.madrid@gmail.com>
2021-12-10 17:10:19 -0300

    movement plis

---
+++ movement.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('/home/pi/mlf-marie-kondo')
+from serial_control import SerialControl
+from mk2robot import MK2Robot
+#from core.serial_control import SerialControl #for pc
+#from core.mk2robot import MK2Robot #for pc
+import time
+import numpy as np
+
+robot_serial = SerialControl()
+robot_serial.open_serial()
+
+class Effector:
+    def __init__(self, a0 = 90, a1 = 40):
+        self.ang = 0
+        self.angmin = a0
+        self.angmax = a1
+
+    def action(self, a=1):
+        if a:
+            #open
+            robot_serial.run_effector(self.angmin)
+        else:
+            #close
+            robot_serial.run_effector(self.angmax)
