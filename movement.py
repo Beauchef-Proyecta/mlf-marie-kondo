fff8a3de6669739d3f540b6792472b88a86fd99d
Leslie Cárdenas Villegas <51496611+leslieecv@users.noreply.github.com>
2022-01-07 13:45:21 -0300

    updated movement imports

--- movement.py
+++ movement.py
@@ -7,8 +7,16 @@ from mk2robot import MK2Robot
 import time
 import numpy as np
 
+robot = MK2Robot(link_lengths=[55, 39, 135, 147, 66.3])
 robot_serial = SerialControl()
 robot_serial.open_serial()
+
+#X_poses = np.array([200, 250, 280, 320])
+#Y_poses = np.array([0, 0, 0, 0])
+#Z_poses = np.array([140, 150, 160, 170])
+
+#q0, q1, q2 = robot.inverse_kinematics(X_poses[i], Y_poses[i], Z_poses[i])
+robot_serial.write_servo(1, 90)
 
 class Effector:
     def __init__(self, a0 = 90, a1 = 40):
@@ -23,3 +31,4 @@ class Effector:
         else:
             #close
             robot_serial.run_effector(self.angmax)
+
