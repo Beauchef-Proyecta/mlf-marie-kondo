412b05d6a76b66b721879e83885a90d3991bb83d
Leslie Cárdenas Villegas <51496611+leslieecv@users.noreply.github.com>
2022-01-07 16:01:01 -0300

    deleted files and created set_angle function in Effector

--- effector.py
+++ effector.py
@@ -10,17 +10,31 @@ robot_serial.open_serial()
 
 class Effector:
     def __init__(self, a0 = 90, a1 = 40):
-        self.ang = 0
         self.angmin = a0
         self.angmax = a1
+        self.ang0 = 90
 
     def action(self, a=1):
         if a:
-            #open
+            #close
             robot_serial.write_servo(4, self.angmin)
         else:
-            #close
+            #open
             robot_serial.write_servo(4, self.angmax)
 
-
-
+    def set_angle(self, ang):
+        if ang>90 or ang<40:
+            print('ang out of range') 
+            #no se como ponerlo como si fuera un error fkjgdg de ahí lo busco
+        else:
+            dif = self.ang0 - ang
+            if dif>0:
+                for i in range(dif + 1):
+                    ang = self.ang0 - i
+                    robot_serial.write_servo(4, ang)
+                    time.sleep(0.05)
+            else: 
+                for i in range(abs(dif) + 1):
+                    ang = 10 + i
+                    robot_serial.write_servo(4, ang)
+                    time.sleep(0.05)
